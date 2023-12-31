61dde4bb01cbe985b7a5e0b5959ad6abf6c7c099
heepdog <heppners@gmail.com>
2023-12-30 21:47:54 -0600

    added cconfig.py for passwords to boot.py

---
+++ boot.py
@@ -0,0 +1,50 @@
+# boot.py -- run on boot-up
+print("hello there2")
+
+
+# Complete project details at https://RandomNerdTutorials.com
+
+import time
+import webrepl
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+import config
+ssid = config.my_sid
+password = config.my_password
+mqtt_server = config.my_mqtt_server
+#EXAMPLE IP ADDRESS
+#mqtt_server = '192.168.1.144'
+global client_id
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'notification'
+topic_pub = b'hello'
+
+last_message = 0
+message_interval = 60
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+print(station.ifconfig())
+
+if station.isconnected() == False:
+    station.active(True)
+    station.connect(ssid, password)
+
+    while station.isconnected() == False:
+        pass
+
+print('Connection successful')
+
+mac = ubinascii.hexlify(station.config('mac'),':').decode()
+
+client_id = f'ESP8266:{client_id.decode("utf-8")}-{mac}'
+
+webrepl.start()
