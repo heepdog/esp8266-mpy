1b21dc0d85c24801d5b32086c6119cd68c6813ba
heepdog <heppners@gmail.com>
2024-01-19 16:36:48 -0600

    esp32 with captive portal

--- boot.py
+++ boot.py
@@ -1,6 +1,4 @@
 # boot.py -- run on boot-up
-print("hello there2")
-
 
 # Complete project details at https://RandomNerdTutorials.com
 
@@ -12,7 +10,7 @@ import machine
 import micropython
 import network
 import esp
-esp.osdebug(None)
+esp.osdebug(None) # turn off vendor O/S debugging messages
 import gc
 gc.collect()
 
@@ -22,7 +20,7 @@ my_ssid_password = config.my_ssid_password
 mqtt_server = config.my_mqtt_server
 #EXAMPLE IP ADDRESS
 #mqtt_server = '192.168.1.144'
-global client_id
+# global client_id, topic_sub, topic_pub
 client_id = ubinascii.hexlify(machine.unique_id())
 topic_sub = b'notification'
 topic_pub = b'hello'
@@ -31,25 +29,29 @@ last_message = 0
 message_interval = 60
 counter = 0
 
-station = network.WLAN(network.STA_IF)
-print(station.ifconfig())
-# station.disconnect()
-# station.active(False)
+# station = network.WLAN(network.STA_IF)
+# print(station.ifconfig())
+# # station.disconnect()
+# # station.active(False)
 
-if station.isconnected() == False:
-    station.active(True)
-    station.connect(ssid, my_ssid_password)
+# if station.isconnected() == False:
+#     station.active(True)
+#     station.connect(ssid, my_ssid_password)
     
     
 
-    while station.isconnected() == False:
-        pass
+#     while station.isconnected() == False:
+#         pass
 
-print('Connection successful')
+# print('Connection successful')
 
-mac = ubinascii.hexlify(station.config('mac'),':').decode()
+from wifi_setup.wifi_setup import WiFiSetup
+ws = WiFiSetup("ding-5cd80b1")
+sta = ws.connect_or_setup()
+
+mac = ubinascii.hexlify(sta.config('mac'),':').decode()
 
 client_id = f'ESP8266:{client_id.decode("utf-8")}-{mac}'
 
 
-#webrepl.start()
+webrepl.start()
--- main.py
+++ main.py
@@ -6,15 +6,23 @@ import micropython
 import network
 import esp
 
+from machine import ADC, Pin
+
 import config
+
+
+
+# import boot
+
 
 def sub_cb(topic, msg):
   print((topic, msg))
   if topic == b'notification' and msg == b'received':
     print('ESP received hello message')
 
-def connect_and_subscribe():
-  global client_id, mqtt_server, topic_sub
+
+def connect_and_subscribe(client_id, mqtt_server, topic_sub):
+  # gloprint topic_sub
 
   client = MQTTClient(client_id,
                       mqtt_server,
@@ -33,22 +41,36 @@ def restart_and_reconnect():
   time.sleep(10)
   machine.reset()
 
-try:
-  client = connect_and_subscribe()
-except OSError as e:
-  restart_and_reconnect()
-
-while True:
+def main():
+  
+  adcx = ADC(Pin(32))
+  adcx.atten(ADC.ATTN_11DB)
+  
+  
+  global last_message, message_interval, counter, topic_pub, msg, client_id, mqtt_server, topic_sub
   try:
-    client.check_msg()
-    if (time.time() - last_message) > message_interval:
-      msg = b'Hello #%d' % counter
-      client.publish(topic_pub, msg)
-      last_message = time.time()
-      counter += 1
+    client = connect_and_subscribe(client_id,mqtt_server,topic_sub)
   except OSError as e:
     restart_and_reconnect()
-  except:
-    
-    client.disconnect()
 
+  while True:
+    try:
+      client.check_msg()
+      temp = adcx.read_uv()
+      
+      if (time.time() - last_message) > message_interval:
+        msg = b'Hello #%d: temp: %d' % (counter,temp)
+        client.publish(topic_pub, msg)
+        last_message = time.time()
+        counter += 1
+    except OSError as e:
+      print (e)
+      restart_and_reconnect()
+    except:
+      
+      client.disconnect()
+      
+
+if(__name__ == '__main__'):
+  print('from main')
+  main()
